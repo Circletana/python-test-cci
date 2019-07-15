1dd489626f418afb43ac6a8e1908a9d51fe85e54
ian-xxiao <ian.xxiao@gmail.com>
2019-07-14 22:45:34 -0400

    - added CircleCI - added pytest

---
+++ cal_score.py
@@ -0,0 +1,10 @@
+# THIS IS THE MAIN CODE
+
+def cal_score(a, b):
+
+    return int(a) * int(b)
+
+
+if __name__ == '__main__':
+
+    print(">>>>> " + str(cal_score(2, 4)))
---
+++ test_calculator_pytest.py
@@ -0,0 +1,5 @@
+from cal_score import cal_score
+
+def test_calculator():
+
+    assert cal_score(2, 4) == 8
