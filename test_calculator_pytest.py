386a87c484520ab4eca9fdd15c55c04f8725ad0d
ian-xxiao <ian.xxiao@gmail.com>
2019-07-14 23:58:39 -0400

    - add addition function - refactor - added new test cases

--- test_calculator_pytest.py
+++ test_calculator_pytest.py
@@ -1,5 +1,9 @@
-from cal_score import cal_score
+from cal_score import cal_addition, cal_multiple
 
-def test_calculator():
+def test_addition():
 
-    assert cal_score(2, 4) == 8
+    assert cal_addition(2, 4) == 6
+
+def test_multiple():
+
+    assert cal_multiple(2, 4) == 8
